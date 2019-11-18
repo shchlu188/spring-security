1f797ccb2cdc13876accb86270a282653efa4a7a
shchlu188@163.com <chenglu8295>
2019-11-18 09:56:13 +0800

    添加测试

--- demo/src/main/java/com/scl/demo/DemoApplication.java
+++ demo/src/main/java/com/scl/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.scl.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String home(){
+		return "hello spring boot";
+	}
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hell word";
+	}
 }
---
+++ demo/src/main/java/com/scl/demo/SpringSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.scl.demo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenglu
+ * Date: 2019/11/17
+ * Description:
+ */
+public class SpringSecurityConfig {
+}
